b4a7f6bc81811c12136113c6a9fd9c48db1a10ae
Augustorresfx <augustorresfx@gmail.com>
2025-01-31 14:16:43 -0300

    changed geojson url to aws

--- glocal/static/js/index.js
+++ glocal/static/js/index.js
@@ -39,7 +39,7 @@ const stars = getStarfield({ numStars: 1000, fog: false });
 scene.add(stars);
 
 // Check for more datasets
-fetch('/glocal/static/geojson/ne_110m_land.json')
+fetch('https://probandodocumentos2.s3.us-west-1.amazonaws.com/geojson/ne_110m_land.json')
   .then(response => response.text())
   .then(text => {
     const data = JSON.parse(text);
