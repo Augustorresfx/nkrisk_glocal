a4a836e76d981948342c63a26a43b7839d22f187
Augustorresfx <augustorresfx@gmail.com>
2025-02-03 18:06:45 -0300

    static error 4

--- nkrisk_glocal/settings.py
+++ nkrisk_glocal/settings.py
@@ -188,16 +188,17 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/glocal/static/'
+# Archivos estáticos (CSS, JS, imágenes) usando WhiteNoise
+STATIC_URL = '/glocal/static/'  # URL pública para los archivos estáticos
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'glocal/static')
+    os.path.join(BASE_DIR, 'glocal/static')  # La ubicación de tus archivos estáticos locales
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Donde se almacenarán los archivos estáticos después de collectstatic
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # Usar WhiteNoise para servir los archivos estáticos
+
 
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
